8240ab2efb66e3dd3ad25e1432f4398605571964
Ryan <clsryan1@gmail.com>
2026-03-19 17:28:57 -0300

    fix: forçando rotas dinamicas para build da vercel

--- src/app/api/produtos/route.ts
+++ src/app/api/produtos/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";import { prisma } from "@/lib/prisma";
+export const dynamic = 'force-dynamic';
 
 export async function GET() {
   try {
